2580fe624d84e4b5fea107a752bb603b200cc5ac
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-19 20:05:43 -0300

    Add User Message -Added Service -Added Controller -Added Route -Added Message in database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import AuthentificateUserController from "./controllers/AuthentificateUserController";
+import CreateMessageController from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middlewares/enssureAuthenticated";
 
 const router = Router();
 router.post('/authenticate', new AuthentificateUserController().handle);
 
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
 export { router }
 
---
+++ server/src/services/CreateMessageService.ts
@@ -0,0 +1,16 @@
+import prismaClient from "../prisma";
+
+export default class CreateMessageService{
+  async execute(text: string, user_id: string){
+    const message = await prismaClient.message.create({
+      data: {
+        text,
+        user_id
+      },
+      include:{
+        user: true
+      },
+    });
+    return message;
+  }
+}
