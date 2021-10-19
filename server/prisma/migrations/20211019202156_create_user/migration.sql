30679ab664eb221e65d148f8c1b1c521f1c320cb
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-19 17:22:49 -0300

    Run Command: yarn prisma migrate dev Added first migration: - Folder 'migrations' with the migration - dev.db and dev.db-jornal - prisma/client dependence - Added User table

---
+++ server/prisma/migrations/20211019202156_create_user/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "github_id" INTEGER NOT NULL,
+    "avatar_url" TEXT NOT NULL,
+    "login" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_github_id_key" ON "users"("github_id");
