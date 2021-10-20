cd42419025d37f7eaa728a0ad5699c759ea6d006
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-20 16:10:14 -0300

    Add style config - Added sass - Added App and global sass stylesheets - Added css Modules - Added assets folder - Added Font family (Roboto, sans-serif)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import style from './App.module.scss'
+
 const App = () => {
 
   return (
-    <div className="App">
+    <main className={style.contentWrapper}>
       <h1>Hello World</h1>
-    </div>
+    </main>
   )
 }
 
