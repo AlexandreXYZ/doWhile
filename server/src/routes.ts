51f2bb657cf629274eec98badd4448270b2c86f4
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-20 00:03:35 -0300

    Add GET method for take 3 last messages

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import AuthentificateUserController from "./controllers/AuthentificateUserController";
 import CreateMessageController from "./controllers/CreateMessageController";
+import {GetLast3MessagesController} from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middlewares/enssureAuthenticated";
 
 const router = Router();
+router.get('/messages/last3', new GetLast3MessagesController().handle);
 router.post('/authenticate', new AuthentificateUserController().handle);
 
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
-export { router }
 
+
+export { router };
--- server/src/services/CreateMessageService.ts
+++ server/src/services/CreateMessageService.ts
@@ -3,25 +3,25 @@ import { io } from "../app";
 
 export default class CreateMessageService{
   async execute(text: string, user_id: string){
-    const message = await prismaClient.message.create({
+    const messages = await prismaClient.message.create({
       data: {
         text,
-        user_id
+        user_id,
       },
       include:{
         user: true
       },
     });
     const infoWS = {
-      text: message.text,
-      user_id: message.user_id,
-      created_at: message.created_at,
+      text: messages.text,
+      user_id: messages.user_id,
+      created_at: messages.created_at,
       user: {
-        name: message.user.name,
-        avatar_url: message.user.avatar_url,
-      }
+        name: messages.user.name,
+        avatar_url: messages.user.avatar_url,
+      },
     }
     io.emit("new_message", infoWS);
-    return message;
+    return messages;
   }
 }
