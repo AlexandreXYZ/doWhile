a8f7bd481e4ddb0f5732171c08c945071011750b
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-20 09:09:12 -0300

    Add Profile User - Added Controller, Service and Route; - Added messages in database.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import AuthentificateUserController from "./controllers/AuthentificateUserController";
 import CreateMessageController from "./controllers/CreateMessageController";
 import {GetLast3MessagesController} from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middlewares/enssureAuthenticated";
 
 const router = Router();
-router.get('/messages/last3', new GetLast3MessagesController().handle);
 router.post('/authenticate', new AuthentificateUserController().handle);
-
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
-
+router.get('/messages/last3', new GetLast3MessagesController().handle);
+router.get('/profile', ensureAuthenticated,new ProfileUserController().handle);
 
 export { router };
