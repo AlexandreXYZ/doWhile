bc027bf9f1a0baf9379512293ed587f88260342b
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-22 18:04:20 -0300

    Home Background Black

--- mobile/src/screens/home/style.ts
+++ mobile/src/screens/home/style.ts
@@ -1,8 +1,10 @@
 import { StyleSheet } from "react-native";
+import { COLORS } from "../../theme";
 
 export const style = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: COLORS.BLACK_SECONDARY,
     
   }
 })
