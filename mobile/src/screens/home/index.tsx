5981d531ae50fe8f7ee0779d28a4c22fb81d064f
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-22 17:10:19 -0300

    Add Home screen

---
+++ mobile/src/screens/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text} from 'react-native'
+import { style } from "./style";
+
+
+export const Home = () => {
+  
+  return (
+    <View style={style.container}>
+      <Text>Hello React Native</Text>
+    </View>
+  )
+}
