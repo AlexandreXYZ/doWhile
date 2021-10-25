9b8e17edab40fb940bfd1a6c467dc28ac163b52f
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-25 08:31:49 -0300

    Add padding-top for Iphone x

--- mobile/src/screens/home/style.ts
+++ mobile/src/screens/home/style.ts
@@ -1,10 +1,11 @@
 import { StyleSheet } from "react-native";
 import { COLORS } from "../../theme";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.BLACK_SECONDARY,
-    
+    paddingTop: getStatusBarHeight() + 16,
   }
 })
